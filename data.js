8efaa05f1ee9224ce37f3a9bbb1e3b12cecfd93f
Fauzan Rafif <fauzanraf@gmail.com>
2025-05-21 22:14:29 +0700

    kelas 2

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
-console.log('fauzan rafif');
+const nama = "Fauzan Rafif";
+let usia = 2;
+
+function generateBiodata() {
+    let generasi;
+    if (usia > 10 && usia < 18) {
+        generasi = "generasi remaja";
+    } else if (usia > 18 && usia < 30) {
+        generasi = "generasi dewasa";
+    } else if (usia > 30) {
+        generasi = "generasi tua"
+    } else if (usia < 10 && usia > 2) {
+        generasi = "generasi anak-anak"
+    } else {
+        generasi = "generasi bayi "
+    }
+    return console.log('generasi saya adalah', generasi);
+}
+
+console.log(nama);
+console.log(usia);
+
+generateBiodata();
